8b3777d1cf623e683569454492fb606798af4d64
HunterRS <hunterrs01@gmail.com>
2022-04-01 15:50:56 -0400

    About+Product+Contact Page added

--- app/app.js
+++ app/app.js
@@ -1,33 +1,83 @@
 var homeContent = `            <div class="hero"></div>
 <div class="content">
-    <h1>AAAAAAAAAAAAAAAAA</h1>
-    <div class="products">
-        <div class="product">
-            <div class="prod-image"></div>
-            <div class="prod-desc">
+    <div class="homeproducts">
+        <div class="homeproduct">
+            <div class="homeprod-image"></div>
+            <div class="homeprod-desc">
                 <h4>Big City</h4>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At saepe nostrum fuga, veritatis soluta sapiente rerum officia nihil numquam voluptas nulla odio unde in, voluptatibus ipsam dignissimos ducimus reprehenderit alias sequi. Excepturi quisquam, assumenda quod maiores dicta nam enim itaque voluptate vero ipsam quasi exercitationem temporibus, quam, pariatur labore optio.</p>
             </div>
         </div>
-        <div class="product">
-            <div class="prod-image"></div>
-            <div class="prod-desc">
+        <div class="homeproduct">
+            <div class="homeprod-image"></div>
+            <div class="homeprod-desc">
                 <h4>Big City</h4>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At saepe nostrum fuga, veritatis soluta sapiente rerum officia nihil numquam voluptas nulla odio unde in, voluptatibus ipsam dignissimos ducimus reprehenderit alias sequi. Excepturi quisquam, assumenda quod maiores dicta nam enim itaque voluptate vero ipsam quasi exercitationem temporibus, quam, pariatur labore optio.</p>
             </div>
         </div>
-        <div class="product">
-            <div class="prod-image"></div>
-            <div class="prod-desc">
+        <div class="homeproduct">
+            <div class="homeprod-image"></div>
+            <div class="homeprod-desc">
                 <h4>Big City</h4>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At saepe nostrum fuga, veritatis soluta sapiente rerum officia nihil numquam voluptas nulla odio unde in, voluptatibus ipsam dignissimos ducimus reprehenderit alias sequi. Excepturi quisquam, assumenda quod maiores dicta nam enim itaque voluptate vero ipsam quasi exercitationem temporibus, quam, pariatur labore optio.</p>
             </div>
         </div>
     </div>
 </div>`
-var aboutContent = "<h1>about</h1>"
-var productsContent = "<h1>products</h1>"
-var contactContent = "<h1>contact</h1>"
+var aboutContent = `            <div class="about">
+<div class="aboutbanner">
+
+</div>
+<div class="aboutdesc">
+    <div class="abouttext">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem dolorum possimus rerum temporibus aliquid sapiente vitae nam numquam adipisci alias explicabo culpa rem doloremque at facilis harum, dignissimos similique, laudantium ipsam modi blanditiis unde maxime. Blanditiis dignissimos cupiditate atque minus soluta aliquam, optio, rerum enim explicabo tempore officia deleniti asperiores.</p>
+    </div>
+    <div class="abouttext">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem dolorum possimus rerum temporibus aliquid sapiente vitae nam numquam adipisci alias explicabo culpa rem doloremque at facilis harum, dignissimos similique, laudantium ipsam modi blanditiis unde maxime. Blanditiis dignissimos cupiditate atque minus soluta aliquam, optio, rerum enim explicabo tempore officia deleniti asperiores.</p>
+    </div>
+    <div class="abouttext">
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem dolorum possimus rerum temporibus aliquid sapiente vitae nam numquam adipisci alias explicabo culpa rem doloremque at facilis harum, dignissimos similique, laudantium ipsam modi blanditiis unde maxime. Blanditiis dignissimos cupiditate atque minus soluta aliquam, optio, rerum enim explicabo tempore officia deleniti asperiores.</p>
+    </div>
+</div>
+</div>`
+var productsContent = `<div class="products">
+<div class="productinfo">
+    <div class="productbox">
+        <div class="producttext">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit eius sequi corrupti, tenetur praesentium alias esse repellendus quibusdam sit, excepturi laudantium facere provident enim dicta quos, quis nostrum. Aliquam similique, quibusdam eaque dolorem expedita blanditiis cumque iste fuga neque facere labore! Voluptate doloremque in aspernatur illum consectetur, maxime eum? Non.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit eius sequi corrupti, tenetur praesentium alias esse repellendus quibusdam sit, excepturi laudantium facere provident enim dicta quos, quis nostrum. Aliquam similique, quibusdam eaque dolorem expedita blanditiis cumque iste fuga neque facere labore! Voluptate doloremque in aspernatur illum consectetur, maxime eum? Non.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit eius sequi corrupti, tenetur praesentium alias esse repellendus quibusdam sit, excepturi laudantium facere provident enim dicta quos, quis nostrum. Aliquam similique, quibusdam eaque dolorem expedita blanditiis cumque iste fuga neque facere labore! Voluptate doloremque in aspernatur illum consectetur, maxime eum? Non.</p>
+        </div>
+        <div class="productpic"></div>
+    </div>
+    <div class="productbox">
+        <div class="producttext">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit eius sequi corrupti, tenetur praesentium alias esse repellendus quibusdam sit, excepturi laudantium facere provident enim dicta quos, quis nostrum. Aliquam similique, quibusdam eaque dolorem expedita blanditiis cumque iste fuga neque facere labore! Voluptate doloremque in aspernatur illum consectetur, maxime eum? Non.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit eius sequi corrupti, tenetur praesentium alias esse repellendus quibusdam sit, excepturi laudantium facere provident enim dicta quos, quis nostrum. Aliquam similique, quibusdam eaque dolorem expedita blanditiis cumque iste fuga neque facere labore! Voluptate doloremque in aspernatur illum consectetur, maxime eum? Non.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit eius sequi corrupti, tenetur praesentium alias esse repellendus quibusdam sit, excepturi laudantium facere provident enim dicta quos, quis nostrum. Aliquam similique, quibusdam eaque dolorem expedita blanditiis cumque iste fuga neque facere labore! Voluptate doloremque in aspernatur illum consectetur, maxime eum? Non.</p>
+        </div>
+        <div class="productpic"></div>
+    </div>
+    <div class="productbox">
+        <div class="producttext">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit eius sequi corrupti, tenetur praesentium alias esse repellendus quibusdam sit, excepturi laudantium facere provident enim dicta quos, quis nostrum. Aliquam similique, quibusdam eaque dolorem expedita blanditiis cumque iste fuga neque facere labore! Voluptate doloremque in aspernatur illum consectetur, maxime eum? Non.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit eius sequi corrupti, tenetur praesentium alias esse repellendus quibusdam sit, excepturi laudantium facere provident enim dicta quos, quis nostrum. Aliquam similique, quibusdam eaque dolorem expedita blanditiis cumque iste fuga neque facere labore! Voluptate doloremque in aspernatur illum consectetur, maxime eum? Non.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit eius sequi corrupti, tenetur praesentium alias esse repellendus quibusdam sit, excepturi laudantium facere provident enim dicta quos, quis nostrum. Aliquam similique, quibusdam eaque dolorem expedita blanditiis cumque iste fuga neque facere labore! Voluptate doloremque in aspernatur illum consectetur, maxime eum? Non.</p>
+        </div>
+        <div class="productpic"></div>
+    </div>
+</div>
+</div>`
+var contactsContent = `            <div class="contact">
+<div class="map">
+
+</div>
+<div class="form">
+    <input type="text" name="" id="" placeholder="Name">
+    <input type="text" name="" id="" placeholder="Email">
+    <input type="text" name="" id="" placeholder="Address">
+</div>
+</div>`
 
 function addButtonListeners(){
     $("nav a").click(function(e){
